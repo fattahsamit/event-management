f8ab1c6d450ad7dd4887210930278bd0dcb91294
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-26 20:05:49 +0600

    Banner Carousel

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,9 +1,41 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 const Banner = () => {
     return (
         <div>
-            <h1>Banner</h1>
+            <div className="w-full carousel">
+                <div id="slide1" className="relative w-full carousel-item">
+                    <img src="https://i.ibb.co/DDg2BCg/pexels-syed-qaarif-andrabi-12946949-Custom.jpg" className="w-full " />
+                    {/* <img src="https://i.ibb.co/CVHT29H/A10.jpg" className="w-4/5 " /> */}
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❮</a>
+                        <a href="#slide2" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="relative w-full carousel-item">
+                    <img src="https://i.ibb.co/WVsm3Lg/troy-t-kbi-GSJpi-TKE-unsplash-Custom.jpg" className="w-full " />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❮</a>
+                        <a href="#slide3" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="relative w-full carousel-item">
+                    <img src="https://i.ibb.co/PQJZF5f/PHOTOFINE-51-compressor-Custom.jpg " className="w-full " />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❮</a>
+                        <a href="#slide4" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="relative w-full carousel-item">
+                    <img src="https://i.ibb.co/60PZsmp/pexels-pixabay-265920-Custom.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❮</a>
+                        <a href="#slide1" className="bg-transparent border border-white hover:bg-slate-500 btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="shadow-xl card w-96 bg-base-100">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="justify-end card-actions">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
