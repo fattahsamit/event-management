ccad926304bbe8438f95229d727fad32fc34d546
fattahsamit <fattahsamit@gmail.com>
2023-05-26 12:21:27 +0600

    responsive navbar

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -3,17 +3,70 @@ import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
   return (
-    <nav className="container mx-auto py-5 flex justify-between">
-      <div>
-        <h2>Logo</h2>
+    <nav className="navbar bg-base-100 md:w-3/4 mx-auto sticky top-0 z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <ActiveLink to={"/"}>Home</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to={"/about"}>About</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to={"/booking"}>Booking</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to={"/contact"}>Contact</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to={"/services"}>Services</ActiveLink>
+            </li>
+          </ul>
+        </div>
+        <h2 className="btn btn-ghost text-primary normal-case text-2xl">
+          Event Management
+        </h2>
       </div>
-      <ul className="flex gap-4">
-        <ActiveLink to={"/"}>Home</ActiveLink>
-        <ActiveLink to={"/about"}>About</ActiveLink>
-        <ActiveLink to={"/booking"}>Booking</ActiveLink>
-        <ActiveLink to={"/contact"}>Contact</ActiveLink>
-        <ActiveLink to={"/services"}>Services</ActiveLink>
-      </ul>
+
+      <div className="navbar-end hidden lg:flex text-lg">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <ActiveLink to={"/"}>Home</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to={"/about"}>About</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to={"/booking"}>Booking</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to={"/contact"}>Contact</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to={"/services"}>Services</ActiveLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
