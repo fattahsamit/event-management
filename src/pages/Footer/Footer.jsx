d81ae3773a08ba478ef79dd5a68bfa66f46616f5
shamimsikder <shamimsikder138@gmail.com>
2023-06-17 12:52:11 +0600

    update

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,10 +1,6 @@
 const Footer = () => {
   return (
-<<<<<<< HEAD
-    <footer className="bg-neutral text-neutral-content flex flex-col justify-around">
-=======
     <footer className="bg-neutral text-neutral-content flex flex-col justify-around ">
->>>>>>> 82698155adf68be404de36c5b7ff685553c78869
       <section className="footer container mx-auto m-8">
         <div>
           <span className="footer-title text-xl">Event Management</span>
